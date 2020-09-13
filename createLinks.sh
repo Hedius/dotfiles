f75ee2def252b59c25cc38c0b690db662370a8d7
Hedius <admin@hedius.at>
2020-09-13 13:10:05 +0200

    add initial rofi and dunst config

--- createLinks.sh
+++ createLinks.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ln -s .config/i3 ~/.config/i3
+repoDir=$(pwd)
+ln -s $repoDir/.config/i3 ~/.config/i3
+ln -s $repoDir/.config/dunst ~/.config/dunst
+ln -s $repoDir/.config/rofi ~/.config/rofi
+
