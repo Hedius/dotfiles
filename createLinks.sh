ca8a2c6b966c0115d67fb9c89f871e4bd4dc354d
Hedius <git@hedius.eu>
2023-12-17 22:01:35 +0100

    fix power profile switcher / flameshot

--- createLinks.sh
+++ createLinks.sh
@@ -4,8 +4,13 @@ repoDir=$(pwd)
 ln -svfr .zshrc -t $HOME
 ln -svfr .vimrc -t $HOME
 
+LOCAL_BIN=~/.local/bin
+mkdir $LOCAL_BIN
+ln -svfr powerprofilecycle/powerprofilecycle.sh -t $LOCAL_BIN
+
 cd .config
 # ln -svf $repoDir/.config/i3 -t $HOME/.config
 # ln -svf $repoDir/.config/dunst -t $HOME/.config
 # ln -svf $repoDir/.config/rofi -t $HOME/.config
 ln -svfr i3 dunst rofi sway alacritty waybar -t $HOME/.config
+
